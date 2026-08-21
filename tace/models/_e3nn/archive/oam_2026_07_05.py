9faed4801393c9f208d84ddcddae8f0579866845
xuzemin <xvzemin123@outlook.com>
2026-08-21 20:07:07 +0800

    disable SO3Grid and SO3VstpGrid

--- tace/models/_e3nn/archive/oam_2026_07_05.py
+++ tace/models/_e3nn/archive/oam_2026_07_05.py
@@ -43,6 +43,160 @@ def so3_expand_index(mmax: int, lmax: int) -> tuple[int, torch.Tensor]:
     return num_l_components, expand_index
 
 
+class CoefficientMappingModule(torch.nn.Module):
+    """
+    Helper module for coefficients used to reshape l <--> m and to get coefficients of specific degree or order
+
+    Args:
+        lmax (int):             Maximum degree of the spherical harmonics
+        mmax (int):             Maximum order of the spherical harmonics
+        use_rotate_inv_rescale (bool):
+                                Whether to pre-compute inverse rotation rescale matrices
+    """
+
+    def __init__(self, lmax, mmax, use_rotate_inv_rescale=False):
+        super().__init__()
+
+        self.lmax = lmax
+        self.mmax = mmax
+        self.use_rotate_inv_rescale = use_rotate_inv_rescale
+
+        m_complex = []  # this m belongs to which SO(3) m
+        l_harmonic = []  # this m belongs to which SO(3) l
+        m_harmonic = []  # this m belongs to which SO(2) m
+
+        for l in range(0, self.lmax + 1):
+            mmax = min(self.mmax, l)
+            m = torch.arange(-mmax, mmax + 1).long()
+            m_complex.append(m)
+            m_harmonic.append(torch.abs(m).long())
+            l_harmonic.append(torch.fill(m, l))
+        m_complex = torch.cat(
+            m_complex, dim=0
+        )  # tensor([0, -1, 0, 1, -2, -1, 0, 1, 2, -3, -2, -1, 0, 1, 2, 3])
+        m_harmonic = torch.cat(
+            m_harmonic, dim=0
+        )  # tensor([0, 1, 0, 1, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3])
+        l_harmonic = torch.cat(
+            l_harmonic, dim=0
+        )  # tensor([0, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3])
+
+        num_components = len(l_harmonic)
+        to_m = torch.zeros([num_components, num_components])
+
+        offset = 0
+        for m in range(self.mmax + 1):
+            idx_r, idx_i = self.complex_idx(m, -1, m_complex, l_harmonic)
+            for idx_out, idx_in in enumerate(idx_r):
+                to_m[idx_out + offset, idx_in] = 1.0
+            offset = offset + len(idx_r)
+            for idx_out, idx_in in enumerate(idx_i):
+                to_m[idx_out + offset, idx_in] = 1.0
+            offset = offset + len(idx_i)
+
+        to_m = to_m.detach()
+
+        self.register_buffer("l_harmonic", l_harmonic)
+        self.register_buffer("m_harmonic", m_harmonic)
+        self.register_buffer("m_complex", m_complex)
+        self.register_buffer("to_m", to_m)
+
+        # for `torch.compile()` compatibility
+        self.pre_compute_coefficient_idx()
+        if self.use_rotate_inv_rescale:
+            self.pre_compute_rotate_inv_rescale()
+
+    def complex_idx(self, m, lmax, m_complex, l_harmonic):
+        if lmax == -1:
+            lmax = self.lmax
+
+        indices = torch.arange(len(l_harmonic))
+        mask_r = torch.bitwise_and(l_harmonic.le(lmax), m_complex.eq(m))
+        mask_idx_r = torch.masked_select(indices, mask_r)
+
+        mask_idx_i = torch.tensor([]).long()
+        if m != 0:
+            mask_i = torch.bitwise_and(l_harmonic.le(lmax), m_complex.eq(-m))
+            mask_idx_i = torch.masked_select(indices, mask_i)
+
+        return mask_idx_r, mask_idx_i
+
+    def pre_compute_coefficient_idx(self):
+        for l in range(self.lmax + 1):
+            for m in range(self.lmax + 1):
+                mask = torch.bitwise_and(self.l_harmonic.le(l), self.m_harmonic.le(m))
+                indices = torch.arange(len(mask))
+                mask_indices = torch.masked_select(indices, mask)
+                self.register_buffer(
+                    "coefficient_idx_l{}_m{}".format(l, m), mask_indices
+                )
+        return
+
+    def prepare_coefficient_idx(self) -> list[list[torch.Tensor]]:
+        # idx = lmax, mmax
+        coefficient_idx_list = []
+        for l in range(self.lmax + 1):
+            l_list = []
+            for m in range(self.lmax + 1):
+                l_list.append(
+                    getattr(self, "coefficient_idx_l{}_m{}".format(l, m), None)
+                )
+            coefficient_idx_list.append(l_list)
+        return coefficient_idx_list
+
+    def coefficient_idx(self, lmax, mmax):
+        if lmax > self.lmax or mmax > self.lmax:
+            mask = torch.bitwise_and(self.l_harmonic.le(lmax), self.m_harmonic.le(mmax))
+            indices = torch.arange(len(mask), device=mask.device)
+            mask_indices = torch.masked_select(indices, mask)
+            return mask_indices
+        else:
+            temp = self.prepare_coefficient_idx()
+            return temp[lmax][mmax]
+
+    def pre_compute_rotate_inv_rescale(self):
+        for l in range(self.lmax + 1):
+            for m in range(self.lmax + 1):
+                mask_indices = self.coefficient_idx(l, m)
+                rotate_inv_rescale = torch.ones(
+                    (1, int((l + 1) ** 2), int((l + 1) ** 2))
+                )
+                for l_sub in range(l + 1):
+                    if l_sub <= m:
+                        continue
+                    start_idx = l_sub**2
+                    length = 2 * l_sub + 1
+                    rescale_factor = math.sqrt(length / (2 * m + 1))
+                    rotate_inv_rescale[
+                        :,
+                        start_idx : (start_idx + length),
+                        start_idx : (start_idx + length),
+                    ] = rescale_factor
+                rotate_inv_rescale = rotate_inv_rescale[:, :, mask_indices]
+                self.register_buffer(
+                    "rotate_inv_rescale_l{}_m{}".format(l, m), rotate_inv_rescale
+                )
+        return
+
+    def prepare_rotate_inv_rescale(self):
+        rotate_inv_rescale_list = []
+        for l in range(self.lmax + 1):
+            l_list = []
+            for m in range(self.lmax + 1):
+                l_list.append(
+                    getattr(self, "rotate_inv_rescale_l{}_m{}".format(l, m), None)
+                )
+            rotate_inv_rescale_list.append(l_list)
+        return rotate_inv_rescale_list
+
+    def get_rotate_inv_rescale(self, lmax, mmax):
+        temp = self.prepare_rotate_inv_rescale()
+        return temp[lmax][mmax]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(mmax={self.mmax}, lmax={self.lmax})"
+
+
 class uvSO2MLinear(torch.nn.Module):
     """
     Based on https://github.com/atomicarchitects/equiformer_v3/blob/main/experimental/models/equiformer_v3/so2_ops.py
