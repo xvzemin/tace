291b7127aac73124a71ba319b16b7f40d8ed7d5d
xuzemin <xvzemin123@outlook.com>
2026-09-07 03:43:19 +0800

    add lienar/nonlienar_spin node embedding

--- tace/models/_e3nn/node.py
+++ tace/models/_e3nn/node.py
@@ -4,19 +4,17 @@
 ################################################################################
 
 
+import math
 from typing import Union
 
 import torch
 from e3nn import o3
-from e3nn.nn import Activation
 
-from tace.utils.torch_scatter import scatter_sum
-
-from ..layout import LayoutTransform
 from ..linear import e3nnLinear
-from ..mlp import MLP
+from ..mlp import MLP, get_scaled_activation
 from .base import NodeEmbedding
 from .fused import O3ScatterTensorProduct
+from .o2 import O2ScatterTensorProduct
 
 
 class LinearNodeEmbedding(NodeEmbedding):
@@ -47,6 +45,7 @@ class LinearNodeEmbedding(NodeEmbedding):
         cutoff: Union[torch.Tensor, None],
         wigner,
         wigner_inv: Union[torch.Tensor, None],
+        magnetic_radial_basis: Union[torch.Tensor, None] = None,
     ) -> torch.Tensor:
 
         return self.elem_emb1(node_attrs)
@@ -55,24 +54,21 @@ class LinearNodeEmbedding(NodeEmbedding):
         return repr(self.elem_emb1)
 
 
-class NonLinearNodeEmbedding(NodeEmbedding):
-    """
-    A node embedding module with nonlinear transformation.
-
-    This class applies a nonlinear activation function after a linear
-    projection.
-    """
+class LinearSpinNodeEmbedding(NodeEmbedding):
+    """Linear embed element types + magnetic-moment radial bases."""
 
     def _setup(self) -> None:
-
         self.irreps_out = o3.Irreps(f"{self.num_channel}x0e")
-
-        self.elem_emb1 = e3nnLinear(
+        self.element_embedding = e3nnLinear(
             f"{self.num_elements}x0e",
-            f"{self.num_channel}x0e",
+            self.irreps_out,
             bias=self.bias,
         )
-        self.act1 = Activation(self.elem_emb1.irreps_out, [torch.nn.SiLU()])
+        self.spin_embedding = e3nnLinear(
+            f"{self.num_mag_radial_basis}x0e",
+            self.irreps_out,
+            bias=self.bias,
+        )
 
     def forward(
         self,
@@ -83,9 +79,46 @@ class NonLinearNodeEmbedding(NodeEmbedding):
         cutoff: Union[torch.Tensor, None],
         wigner,
         wigner_inv: Union[torch.Tensor, None],
+        magnetic_radial_basis: Union[torch.Tensor, None] = None,
     ) -> torch.Tensor:
+        if magnetic_radial_basis is None:
+            raise ValueError("LinearSpinNodeEmbedding requires magnetic radial bases.")
+        return (
+            self.element_embedding(node_attrs)
+            + self.spin_embedding(magnetic_radial_basis)
+        ) / math.sqrt(2.0)
 
-        return self.act1(self.elem_emb1(node_attrs))
+
+class NonLinearSpinNodeEmbedding(LinearSpinNodeEmbedding):
+    """Embed element and spin inputs, followed by a scaled SiLU."""
+
+    def _setup(self) -> None:
+        super()._setup()
+        self.activation = get_scaled_activation("silu")
+
+    def forward(
+        self,
+        node_attrs: torch.Tensor,
+        edge_feats: torch.Tensor,
+        edge_index: torch.Tensor,
+        edge_attrs: torch.Tensor,
+        cutoff: Union[torch.Tensor, None],
+        wigner,
+        wigner_inv: Union[torch.Tensor, None],
+        magnetic_radial_basis: Union[torch.Tensor, None] = None,
+    ) -> torch.Tensor:
+        return self.activation(
+            super().forward(
+                node_attrs,
+                edge_feats,
+                edge_index,
+                edge_attrs,
+                cutoff,
+                wigner,
+                wigner_inv,
+                magnetic_radial_basis,
+            )
+        )
 
 
 class TensorNodeEmbedding(NodeEmbedding):
@@ -131,6 +164,7 @@ class TensorNodeEmbedding(NodeEmbedding):
         cutoff: Union[torch.Tensor, None],
         wigner,
         wigner_inv: Union[torch.Tensor, None],
+        magnetic_radial_basis: Union[torch.Tensor, None] = None,
     ) -> torch.Tensor:
 
         base_node_feats = self.node_embedding(node_attrs)
@@ -159,9 +193,8 @@ class TensorNodeEmbedding(NodeEmbedding):
         return node_feats
 
 
-class SO2TensorNodeEmbedding(NodeEmbedding):
+class O2TensorNodeEmbedding(NodeEmbedding):
     def _setup(self) -> None:
-
         self.node_embedding = e3nnLinear(
             f"{self.num_elements}x0e", f"{self.num_channel}x0e", bias=self.bias
         )
@@ -173,20 +206,32 @@ class SO2TensorNodeEmbedding(NodeEmbedding):
         )
         torch.nn.init.uniform_(self.source_embedding.weight, a=-0.001, b=0.001)
         torch.nn.init.uniform_(self.target_embedding.weight, a=-0.001, b=0.001)
+        self.irreps_out = o3.Irreps(
+            [(self.num_channel, (l, (-1) ** l)) for l in range(self.Lmax + 1)]
+        )
+        self.rejector = O2ScatterTensorProduct(
+            self.node_embedding.irreps_out,
+            self.irreps_out,
+            num_channel=self.num_channel,
+            lmax=max(self.Lmax, self.lmax),
+            mmax=0,
+            even_scalar_act=torch.nn.SiLU(),
+            odd_scalar_act=torch.nn.Tanh(),
+            tensor_act=torch.nn.Sigmoid(),
+            num_head=1,
+            num_radial_basis=self.num_radial_basis,
+            use_radial_rotary_attention=False,
+        )
         self.edge_info = MLP(
             channels=[
                 self.num_radial_basis + self.num_channel * 2,
                 self.num_channel,
                 self.num_channel,
-                (self.Lmax + 1) * self.num_channel,
+                self.rejector.weight_numel,
             ],
             bias=True,
             layer_norm=True,
         )
-        self.irreps_out = o3.Irreps(
-            [(self.num_channel, (l, (-1) ** l)) for l in range(self.Lmax + 1)]
-        )
-        self.reshape = LayoutTransform(self.irreps_out)
 
     def forward(
         self,
@@ -197,41 +242,40 @@ class SO2TensorNodeEmbedding(NodeEmbedding):
         cutoff: Union[torch.Tensor, None],
         wigner,
         wigner_inv: Union[torch.Tensor, None],
+        magnetic_radial_basis: Union[torch.Tensor, None] = None,
     ) -> torch.Tensor:
 
         base_node_feats = self.node_embedding(node_attrs)
         source_feats = self.source_embedding(node_attrs[edge_index[0]])
         target_feats = self.target_embedding(node_attrs[edge_index[1]])
-        edge_feats = self.edge_info(
+        conv_weights = self.edge_info(
             torch.cat([edge_feats, source_feats, target_feats], dim=-1)
         )
-        if cutoff is not None:
-            edge_feats = edge_feats * cutoff
-
-        edge_feats = edge_feats.view(
-            edge_feats.size(0), (self.Lmax + 1), self.num_channel
-        )
-        edge_feats = torch.bmm(
-            self.so2_angular_basis.wigner_inv.narrow(2, 0, (self.lmax + 1)), edge_feats
-        )  # (edge, so3_m, C)
-
         node_feats = (
-            scatter_sum(
-                edge_feats, edge_index[1], dim=0, dim_size=base_node_feats.size(0)
+            self.rejector(
+                torch.ones_like(base_node_feats),
+                conv_weights,
+                edge_index,
+                wigner,
+                wigner_inv,
+                edge_cutoff=(
+                    cutoff
+                    if cutoff is not None
+                    else edge_feats.new_ones(edge_feats.size(0), 1)
+                ),
             )
             / self.avg_num_neighbors
         )
-
-        node_feats[:, 0:1, :] = node_feats.narrow(1, 0, 1) + base_node_feats.unsqueeze(
-            1
+        node_feats[:, : self.num_channel] = (
+            node_feats.narrow(1, 0, self.num_channel) + base_node_feats
         )
-
-        return self.reshape.inverse(node_feats)
+        return node_feats
 
 
 NODE_EMBEDDING = {
     "linear": LinearNodeEmbedding,
-    "nonlinear": NonLinearNodeEmbedding,
+    "linear_spin": LinearSpinNodeEmbedding,
+    "nonlinear_spin": NonLinearSpinNodeEmbedding,
     "tensor": TensorNodeEmbedding,
-    "so2_tensor": SO2TensorNodeEmbedding,
+    "o2_tensor": O2TensorNodeEmbedding,
 }
